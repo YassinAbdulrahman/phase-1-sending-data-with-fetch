618c11e2d4853ec5fdf3e107b59424d3dd05b4be
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-02-19 11:34:11 +0300

    Done

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 // Add your code here
-let forminput = document.querySelector("form")
 
-forminput.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let name = document.querySelector("#name")
-    let email = document.querySelector("#email")
-    submitData(name, email)
-})
+let forminput = document.querySelector("form");
+
+forminput.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let name = document.getElementById("userName").value;
+  let email = document.getElementById("userMail").value;
+  submitData(name, email);
+});
 
 function submitData(name, email) {
-    console.log(name.value)
-    console.log(email.value)
-    fetch('http://localhost:3000/users', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        body: JSON.stringify(
-            {
-                name:name.value,
-                email:email.value,
-            }
-        ),
+  console.log(name);
+  console.log(email);
+ return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      let heading = document.createElement("h3");
+      heading.textContent = data['id'];
+      forminput.appendChild(heading);
     })
-    .then(res => {console.log(res); return res.json();})
-    .then(data => console.log(data))
+    .catch(function (error) {
+        let p = document.createElement("p");
+        p.textContent = error.message
+        forminput.appendChild(p)
+      });
 }
-
